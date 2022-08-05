de77d114dc3e1a509d00f8fe3166bda097fedcc5
LeeJinKyum <109436439+LeeJinKyum@users.noreply.github.com>
2022-08-05 23:10:46 +0900

    wrong loc

--- weather.js
+++ weather.js
@@ -1,15 +1,16 @@
-const API_KEY = "1e253e8a220be687711103addcd27982";
-
 function onGeoOk(positon) {
-    const lat = positon.coords.latitude;    
+    const lat = positon.coords.latitude;
     const lon = positon.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url).then(response => response.json().then(data => {
+    const API = "060a5777776159e2661dbc7379bb81f5";
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
         city.innerText = data.name;
         weather.innerText = data.weather[0].main;
-    }));
+    });
 }
 
 function onGeoError() {
